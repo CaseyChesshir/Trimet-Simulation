5c9c1dd1ace6797c60fa7b895c3fa22fc3a4266d
CaseyChesshir <chesshir@uoregon.edu>
2017-06-18 21:56:11 -0700

    Initial Commit

---
+++ Trimet Simulation/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Trimet Simulation
+//
+//  Created by Casey Chesshir on 6/18/17.
+//  Copyright © 2017 Chesshir Development. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
